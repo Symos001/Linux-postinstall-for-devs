fc4e7e845aa63b0217f70fd9ea4b52b2fdb9502a
Symos001 <123610193+Symos001@users.noreply.github.com>
2023-06-02 21:47:24 -0300

    Correção Scripts-posinstalação linuxmint.sh

--- Scripts-posinstalação linuxmint.sh
+++ Scripts-posinstalação linuxmint.sh
@@ -121,15 +121,6 @@ sudo apt install mysql-server
 
 
 
-##instalando o mongoDB##
-sudo apt update && sudo apt install gnupg -y
-wget -qO - https: // www.mongodb.org / static / pgp / server- 4.2 .asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/
-mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list _ _ _ _ _ _ _ _ _
-sudo apt update
-sudo apt install mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
 
 ##instalando docker##
 sudo apt-get update
